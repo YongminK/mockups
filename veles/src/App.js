cc5291e77fad5c782213c8c26ee19b428d7d2498
Елена Петрова <e.petrova@int.robotbull.com>
2020-02-11 15:35:39 +0300

    деактивация скрола контента при открытии меню

--- veles/src/App.js
+++ veles/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import MainPage from "./screens/MainPage";
 import './styles/main-styles.sass';
 import './styles/main-page.sass';
+
 function App() {
-  return (
-    <div className="container">
-      <MainPage/>
-    </div>
-  );
+    const [showMobileMenu, setShowMobileMenu] = React.useState(false)
+    return (
+        <div className={showMobileMenu ? "container overflow-hidden" : "container"}>
+            <MainPage showMobileMenu={showMobileMenu} setShowMobileMenu={setShowMobileMenu}/>
+        </div>
+    );
 }
 
 export default App;
