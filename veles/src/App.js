6316b3cc0f694bf0a462b956802cfd5b7e3a9e43
Елена Петрова <e.petrova@int.robotbull.com>
2020-02-11 14:35:24 +0300

    подключение MainPage и стилей в App

--- veles/src/App.js
+++ veles/src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
-
+import MainPage from "./screens/MainPage";
+import './styles/main-styles.sass';
+import './styles/main-page.sass';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <MainPage/>
     </div>
   );
 }
