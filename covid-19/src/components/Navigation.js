bcd361214e03d9b3119e9c0c71f57f0b1fd328d1
Елена Петрова <e.petrova@int.robotbull.com>
2020-03-31 00:52:07 +0300

    header padding

--- covid-19/src/components/Navigation.js
+++ covid-19/src/components/Navigation.js
@@ -2,7 +2,7 @@ import React from "react";
 import '../styles/navigation.sass'
 export default function Navigation() {
     return(
-        <div className='navigation header'>
+        <div className='navigation top'>
             <div className='navigation__logo'>
                 <img src={require('../img/virus-icon.png')} alt=""/>
                 <span>COVID-19</span>
