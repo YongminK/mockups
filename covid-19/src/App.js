2527eeddd043ae39c7061fa82e769ba17a112d73
Елена Петрова <e.petrova@int.robotbull.com>
2020-03-31 00:52:44 +0300

    overview block

--- covid-19/src/App.js
+++ covid-19/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './styles/main-style.sass'
 import Navigation from "./components/Navigation";
+import Header from "./components/Header";
+import Overview from "./components/Overview";
 function App() {
   return (
-    <div>
+    <>
       <Navigation/>
-    </div>
+      <Header/>
+      <Overview/>
+    </>
   );
 }
 
---
+++ covid-19/src/components/Overview.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Overview() {
+    return (
+        <section className='overview' id='overview'>
+            <div className='content'>
+                <p className="title-red">What is Covid-19</p>
+                <p className="title-green">Coronavirus</p>
+                <p className="text">
+                    Corona viruses are a type of virus. There are many different kinds, and some cause disease. A newly
+                    identified type has caused a recent outbreak of respiratory illness now called COVID-19.Lauren Sauer,
+                    M.S., the director of operations with the Johns Hopkins Office of Critical Event Preparedness and
+                    Response
+                </p>
+                <button>
+                    Learn More
+                </button>
+            </div>
+        </section>
+    )
+}
