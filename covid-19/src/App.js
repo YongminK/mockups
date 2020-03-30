8277ad48fc68fa1bcb9c94d493111ebdddcaf4c6
Елена Петрова <e.petrova@int.robotbull.com>
2020-03-31 02:28:35 +0300

    Update App.js

--- covid-19/src/App.js
+++ covid-19/src/App.js
@@ -3,12 +3,18 @@ import './styles/main-style.sass'
 import Navigation from "./components/Navigation";
 import Header from "./components/Header";
 import Overview from "./components/Overview";
+import Contagion from "./components/Contagion";
+import Symptomps from "./components/Symptomps";
+import WhatShouldWeDo from "./components/WhatShouldWeDo";
 function App() {
   return (
     <>
       <Navigation/>
       <Header/>
       <Overview/>
+      <Contagion/>
+      <Symptomps/>
+      <WhatShouldWeDo/>
     </>
   );
 }
