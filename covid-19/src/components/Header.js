b5df7bbf08e23698c6af97a8d0294e88dc32e242
Елена Петрова <e.petrova@int.robotbull.com>
2020-03-31 00:52:53 +0300

    header block

---
+++ covid-19/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <section className='header'>
+            <p className='title-red'>
+                COVID-19 Alert
+            </p>
+            <p className='title-green'>
+                Stay at Home quarantine To stop Corona virus
+            </p>
+            <p className='text'>
+                There is no specific medicine to prevent or treat coronavirus disease (COVID-19). People may need
+                supportive care to .
+            </p>
+            <button>
+                Let Us Help
+            </button>
+        </section>
+    )
+}
