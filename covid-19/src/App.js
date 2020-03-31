5cea53051456b341fa464099433322cbd9b023f5
Елена Петрова <e.petrova@int.robotbull.com>
2020-03-31 05:00:26 +0300

    Update App.js

--- covid-19/src/App.js
+++ covid-19/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './styles/main-style.sass'
 import Navigation from "./components/Navigation";
 import Header from "./components/Header";
 import Overview from "./components/Overview";
